67996c19b5645cc78e1c87abdaff546d9f341b4e
yousef moien hussain <zeroyousef13@gmail.com>
2022-03-05 13:39:34 +0300

    yousef cw2-day2

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var greeting = "Hello, playground"
+var hobby="FOOTBALL"
+let name="yousef"
+let favouriteNumber=16
+print("jfmj  1+1 \(name)my hobby is \(hobby)jfmh my favouritenumber is \(favouriteNumber)")
+var age=17
+var height=1.70
+
+
